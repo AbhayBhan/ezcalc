f4bbb04d3056fb37c28aa6a990a3f6249fa90a1f
AbhayBhan <abhaybhan197@gmail.com>
2022-11-03 19:59:03 +0530

    Initial Setup Done

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './css/main.css';
+
+function App() {
+  return (
+    <div className="flex flex-row container justify-center mx-auto">
+      Hello
+    </div>
+  );
+}
+
+export default App;
